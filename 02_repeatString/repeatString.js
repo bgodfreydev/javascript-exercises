65feb18842537404c76f6742d4a61ae8280369be
Brian <brian.godfrey@gmail.com>
2025-02-12 08:16:12 -0500

    Finished 01 and 02.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, count) {
+  let newString = "";
+  if (count < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < count; i++) {
+      newString = newString + word;
+    }
+    return newString;
+  }
 };
 
 // Do not edit below this line
