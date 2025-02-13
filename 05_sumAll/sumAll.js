29c5634bff9ffd77601165d0120f4a4291e12f4b
Brian <brian.godfrey@gmail.com>
2025-02-13 05:41:36 -0500

    Completed 05, 06, and 07.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let num3 = 0;
+  let total = 0;
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  }
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  } else if (num1 < num2) {
+    num1 = num1;
+    num3 = num2;
+  } else {
+    num3 = num1;
+    num1 = num2;
+  }
+  for (let i = num1; i <= num3; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+function roundTo1Decimal(number) {
+  return Math.round(number * 10) / 10;
+}
+
+const convertToCelsius = function (temp) {
+  return roundTo1Decimal(((temp - 32) * 5) / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  return roundTo1Decimal((temp * 9) / 5 + 32);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
